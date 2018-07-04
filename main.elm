ba226382f63793f3f9ff1eb1d786a7c7383ce41e
Sune Scheuerlein-Tougaard <stxa@novozymes.com>
2018-07-04 09:29:04 +0200

    initial app

---
+++ main.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput)
+import Html exposing (..)
+
+
+-- MODEL
+
+
+type alias Model =
+    { message : String }
+
+
+
+-- UPDATE
+
+
+isEven : Int -> Bool
+isEven int =
+    int % 2 == 0
+
+
+toMessage : Result String Bool -> String
+toMessage res =
+    case res of
+        Ok True ->
+            "That is an even number"
+
+        Ok False ->
+            "That is not an even number"
+
+        Err str ->
+            str
+
+
+checkIsEven : String -> String
+checkIsEven =
+    String.trim >> String.toInt >> Result.map isEven >> toMessage
+
+
+type Msg
+    = Check String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Check val ->
+            { model | message = checkIsEven val }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text (model.message) ]
+        , input [ placeholder "Enter number to check here", onInput Check ] []
+        ]
+
+
+main =
+    Html.beginnerProgram
+        { model = { message = "" }
+        , view = view
+        , update = update
+        }
